330bd25c2b53add97bdd32322a7d0da5f980b118
PrimaryKi ;D <51946019+primaryki@users.noreply.github.com>
2024-02-01 17:11:41 -0500

    Added prework files to repo

---
+++ PreWork Project/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  PreWork Project
+//
+//  Created by Kiana Moore on 1/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) { let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    func changeColor() -> UIColor{
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red,green: green, blue: blue, alpha:0.5)
+    }
+}
+
+
